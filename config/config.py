b43debac3ce7d28898db8aa4fe08c79ae807385a
xxDr34dxx <127802752+xxDr34dxx@users.noreply.github.com>
2023-03-20 15:49:32 -0700

    Create config.py
    Initial commit

---
+++ config/config.py
@@ -0,0 +1,7 @@
+# The following are MANDATORY for the bot to function
+BOT_TOKEN = '<replace me>'
+GUILDID = '<replace me>' #Must be numeic server ID from Discord
+LOG_CHANNEL = '<replace me>' #Must be numeic channel ID from Discord
+DEFAULTURL = 'https://ice4.somafm.com/defcon-128-mp3' #Must be media format FFMPEG understands
+STATUSCHAN = '<replace me>' #Must be numeic channel ID from Discord
+VCHANNEL = '<replace me>' #Must be numeic channel ID from Discord
